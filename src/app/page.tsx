f43835d5c49519dfe656e5fc949ca69265c79638
Camilo Nossa <calejandro_noss@outlook.com>
2024-09-10 15:16:35 -0500

    ♻️ Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-"use client";
-
-import { useState, useEffect } from 'react';
-import { ThemeSwitcher } from '@/components/theme-switcher';
-import { User } from '@/app/api/users/interfaces/user.interface';
+import { ThemeSwitcher } from '@/components/theme-switcher'
 
 export default function Home() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    fetch('/api/users')
-      .then((res) => res.json())
-      .then((data) => setUsers(data.users))
-      .catch((err) => console.error('Error fetching users:', err));
-  }, []);
-
   return (
     <main className="flex min-h-full flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold">Hello World!</h1>
@@ -21,25 +8,6 @@ export default function Home() {
       <div className="flex gap-4 justify-center">
         <ThemeSwitcher variant="flat" />
       </div>
-
-      <div>
-        <h1>Users</h1>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              <p>{user.email} {user.firstName} {user.lastName}</p>
-              <ul>
-                {user.posts.map((post) => (
-                  <li key={post.id}>
-                    <p><strong>Title:</strong> {post.title}</p>
-                    <p><strong>Content:</strong> {post.content}</p>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </div>
     </main>
-  );
+  )
 }
