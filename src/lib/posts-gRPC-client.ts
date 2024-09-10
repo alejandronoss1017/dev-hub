2e5db1f7a18b924ab1f817f306ddd2c48e0123c5
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-09-10 15:35:04 -0500

    ✨ Add posts CRUD with gRPC

--- src/lib/posts-gRPC-client.ts
+++ src/lib/posts-gRPC-client.ts
@@ -3,6 +3,8 @@
 import { Post } from '@/types'
 import { loadPackageDefinition, credentials } from '@grpc/grpc-js'
 import { loadSync } from '@grpc/proto-loader'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import path from 'path'
 
 const PROTO_PATH = path.resolve(process.cwd(), 'src/proto/posts.proto')
@@ -25,16 +27,84 @@ const client = new (postsProto as any).PostsService(
   credentials.createInsecure()
 )
 
-async function fetchPosts(): Promise<Post[]> {
+export async function fetchPosts(): Promise<Post[]> {
   return new Promise((resolve, reject) => {
     client.FindAll({}, (error: any, response: any) => {
       if (error) {
         reject(error)
       } else {
-        resolve(response.users)
+        resolve(response.posts)
       }
     })
   })
 }
 
-export { fetchPosts }
+export async function fetchPost(id: number): Promise<Post> {
+  return new Promise((resolve, reject) => {
+    client.FindOne({ id }, (error: any, response: any) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(response)
+      }
+    })
+  })
+}
+
+export async function createPost(formData: FormData): Promise<void> {
+  const title = formData.get('title') as string
+  const content = formData.get('content') as string
+  const authorId = parseInt(formData.get('authorId') as string, 10)
+
+  await new Promise((resolve, reject) => {
+    client.Create(
+      { title, content, authorId },
+      (error: any, response: any) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(response.post)
+        }
+      }
+    )
+  })
+
+  revalidatePath('/posts')
+  redirect('/posts')
+}
+
+export async function updatePost(formData: FormData): Promise<void> {
+  const id = parseInt(formData.get('id') as string, 10)
+  const title = formData.get('title') as string
+  const content = formData.get('content') as string
+
+  await new Promise((resolve, reject) => {
+    client.Update(
+      { id, title, content },
+      (error: any, response: any) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(response.post)
+        }
+      }
+    )
+  })
+
+  revalidatePath(`/posts/${id}`)
+  redirect(`/posts`)
+}
+
+export async function removePost(id: number, path: string): Promise<void> {
+  await new Promise((resolve, reject) => {
+    client.Remove({ id }, (error: Error | null, response: any) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(response)
+      }
+    })
+  })
+
+  revalidatePath(path)
+}
